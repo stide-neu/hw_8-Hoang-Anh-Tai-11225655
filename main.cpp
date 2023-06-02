54deb17526a5db2f1586e05ee6a26edbc4223330
Hoang-Anh-Tai-11225655 <128773369+Hoang-Anh-Tai-11225655@users.noreply.github.com>
2023-06-02 09:27:42 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,13 +1,51 @@
-/******************************************************************************
-Copy đoạn code vào file này
-**Lưu ý không đổi tên file hoặc định dạng file
-**Nộp thành công sẽ có tích xanh ở ngoài repository
-*******************************************************************************/
-#include <stdio.h>
+#include<stdio.h>
+struct Student{
+    char name [20];
+    int eng;
+    int math;
+    int phys;
+    
+};
 
-int main()
-{
-    printf("Hello World");
-
-    return 0;
+int main(){
+    struct Student Student[3]; 
+    for(int i=0 ; i<3;i++){
+        printf( "input name"),scanf("%s",Student[i].name);
+        printf( "input math"),scanf("%d",&Student[i].math);
+        printf( "input eng"),scanf("%d",&Student[i].eng);
+        printf( "input phys"),scanf("%d",&Student[i].phys);
+        printf("\n");
+    }
+    for( int i=0;i<3;i++){
+        printf("name of student a %s \n",Student[i].name);
+        printf("math grade of student %d\n",Student[i].math);
+        printf("eng grade of student %d\n",Student[i].eng);
+        printf("phys grade  of student %d\n",Student[i].phys);
+        printf("\n");
+    }
+    float mean;
+    int math;
+    int eng;
+    int phys;
+     for(int i = 0; i < 3; i++){
+        mean =(math+eng+phys)/3;
+        if(90<=mean<=100){
+            printf("student %s achieve grade S\n",Student[i].name);
+        }
+         else if (80<=mean<90){
+            printf("student %s achieve grade A\n",Student[i].name);
+        }
+         else if(70<=mean<80){
+            printf("student %s achieve grade B\n",Student[i].name);
+        }
+         else if(60<=mean<70){
+            printf("student %s achieve grade C\n",Student[i].name);
+        }
+        else{
+            printf("student %s achieve grade D\n",Student[i].name);
+        }
+     }
 }
+
+    
+    
